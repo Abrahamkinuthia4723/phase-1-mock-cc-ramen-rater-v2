d34ac76bc26418e63824463a555d95e7d2949ce0
Abraham Kinuthia <abrahamkinuthia193@gmail.com>
2025-03-16 16:55:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,23 +2,58 @@
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  document.querySelector("#ramen-detail .detail-image").src = ramen.image;
+  document.querySelector("#ramen-detail .detail-image").alt = ramen.name;
+  document.querySelector("#ramen-detail .name").innerText = ramen.name;
+  document.querySelector("#ramen-detail .restaurant").innerText = ramen.restaurant;
+  document.getElementById("rating-display").innerText = ramen.rating;
+  document.getElementById("comment-display").innerText = ramen.comment;
+};
+
+const displayRamen = (ramenItem) => {
+  const ramenMenu = document.getElementById("ramen-menu");
+  const ramenImg = document.createElement("img");
+  ramenImg.src = ramenItem.image;
+  ramenImg.alt = ramenItem.name;
+  ramenImg.classList.add("image-slider");
+  ramenImg.addEventListener("click", () => handleClick(ramenItem));
+  ramenMenu.appendChild(ramenImg);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const newRamenItem = {
+    name: form["new-name"].value,
+    restaurant: form.restaurant.value,
+    image: form.image.value,
+    rating: form.rating.value,
+    comment: form["new-comment"].value,
+  };
+  displayRamen(newRamenItem);
+  form.reset();
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  document.querySelector("#new-ramen").addEventListener("submit", handleSubmit);
+};
 
 const displayRamens = () => {
-  // Add code
+  fetch("http://localhost:3000/ramens")
+    .then((response) => response.json())
+    .then((ramens) => {
+      document.getElementById("ramen-menu").innerHTML = "";
+      ramens.forEach(displayRamen);
+    })
+    .catch(console.error);
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+  displayRamens();
+  addSubmitListener();
+};
 
-main()
+main();
 
 // Export functions for testing
 export {
